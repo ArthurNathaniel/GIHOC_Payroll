fa863cd53a812e1534078b01c90b5c106d8277f8
ArthurNathaniel <nathanielk.larthur@gmail.com>
2024-12-11 06:36:12 +0000

    updates

--- db.php
+++ db.php
@@ -1,14 +1,11 @@
 <?php
-// $host = 'localhost';
-// $db = 'havenpay'; // Replace with your database name
-// $user = 'root'; // Replace with your database username
-// $pass = ''; // Replace with your database password
+$host = 'localhost';
+$db = 'gihocpay'; // Replace with your database name
+$user = 'root'; // Replace with your database username
+$pass = ''; // Replace with your database password
 
 
-$host = 'therubhaven.com';
-$db = 'u500921674_havenpay'; // Replace with your database name
-$user = 'u500921674_havenpay'; // Replace with your database username
-$pass = 'OnGod@123'; // Replace with your database password
+
 // Create connection
 $conn = new mysqli($host, $user, $pass, $db);
 
